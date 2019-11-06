a3ddf99a196af22e10081ed0e79fd3f868bbdf93
Xianyu Li <lix113@newschool.edu>
2019-11-05 22:06:07 -0500

    initial push

---
+++ js/script.js
@@ -0,0 +1,120 @@
+const userInput = window.prompt('Give me an adjective');
+const userOutputEl = document.getElementById('output');
+
+console.log(userInput);
+console.log(userOutputEl);
+
+const userInputAdj = window.prompt('Give me another adjective');
+const userOutputElAdj = document.getElementById('output');
+
+console.log(userInputAdj);
+console.log(userOutputElAdj);
+
+const userInputNoun = window.prompt('Give me a noun');
+const userOutputElNoun = document.getElementById('output');
+
+console.log(userInputNoun);
+console.log(userOutputElNoun);
+
+const userInputNoun2 = window.prompt('Give me another noun');
+const userOutputElNoun2 = document.getElementById('output');
+
+console.log(userInputNoun2);
+console.log(userOutputElNoun2);
+
+const userInputPluralNoun = window.prompt('Give me a plural noun');
+const userOutputElPluralNoun = document.getElementById('output');
+
+console.log(userInputPluralNoun);
+console.log(userOutputElPluralNoun);
+
+const userInputGame = window.prompt('Give me a game');
+const userOutputElGame = document.getElementById('output');
+
+console.log(userInputGame);
+console.log(userOutputElGame);
+
+const userInputPluralNoun2 = window.prompt('Give me a plural noun');
+const userOutputElPluralNoun2 = document.getElementById('output');
+
+console.log(userInputPluralNoun2);
+console.log(userOutputElPluralNoun2);
+
+const userInputVerbIng = window.prompt('Give me a verb ending in"-ing"');
+const userOutputElVerbIng = document.getElementById('output');
+
+console.log(userInputVerbIng);
+console.log(userOutputElVerbIng);
+
+const userInputVerbIng2 = window.prompt('Give me another verb ending in"-ing"');
+const userOutputElVerbIng2 = document.getElementById('output');
+
+console.log(userInputVerbIng2);
+console.log(userOutputElVerbIng2);
+
+const userInputPluralNoun3 = window.prompt('Give me a plural noun');
+const userOutputElPluralNoun3 = document.getElementById('output');
+
+console.log(userInputPluralNoun3);
+console.log(userOutputElPluralNoun3);
+
+const userInputVerbIng3 = window.prompt('Give me another verb ending in"-ing"');
+const userOutputElVerbIng3 = document.getElementById('output');
+
+console.log(userInputVerbIng3);
+console.log(userOutputElVerbIng3);
+
+const userInputNoun3 = window.prompt('Give me another noun');
+const userOutputElNoun3 = document.getElementById('output');
+
+console.log(userInputNoun3);
+console.log(userOutputElNoun3);
+
+const userInputPlant = window.prompt('Give me a plant');
+const userOutputElPlant = document.getElementById('output');
+
+console.log(userInputPlant);
+console.log(userOutputElPlant);
+
+const userInputBody = window.prompt('Give me a part of a body');
+const userOutputElBody = document.getElementById('output');
+
+console.log(userInputBody);
+console.log(userOutputElBody);
+
+const userInputPlace = window.prompt('Give me a place');
+const userOutputElPlace = document.getElementById('output');
+
+console.log(userInputPlace);
+console.log(userOutputElPlace);
+
+const userInputVerbIng4 = window.prompt('Give me another verb ending in"-ing"');
+const userOutputElVerbIng4 = document.getElementById('output');
+
+console.log(userInputVerbIng4);
+console.log(userOutputElVerbIng4);
+
+const userInputAdj2 = window.prompt('Give me an adjective');
+const userOutputElAdj2 = document.getElementById('output');
+
+console.log(userInputAdj2);
+console.log(userOutputElAdj2);
+
+const userInputNumber = window.prompt('Give me a number');
+const userOutputElNumber= document.getElementById('output');
+
+console.log(userInputNumber);
+console.log(userOutputElNumber);
+
+const userInputPluralNoun4 = window.prompt('Give me a plural noun');
+const userOutputElPluralNoun4 = document.getElementById('output');
+
+console.log(userInputPluralNoun4);
+console.log(userOutputElPluralNoun4);
+
+// userOutputEl.innerText = "Hello, " + userInput;
+// userOutputEl.innerText= `Hello, ${userInput}!`;
+
+// Alternative Solution
+userOutputEl.innerHTML = `<h1>A vacation is when you take a trip to some ${userInput} place with your ${userInputAdj} family. Usually you go to some place that is near a/an ${userInputNoun} or up on a/an ${userInputNoun2}. A good vacation place is one where you can ride ${userInputPluralNoun} or play ${userInputGame} or go hunting for ${userInputPluralNoun2}. I like to spend my time ${userInputVerbIng} or ${userInputVerbIng2}. When parents go on vacation, they spend their time eating three ${userInputPluralNoun3} a day, and fathers play golf, and mothers sit around ${userInputVerbIng3}. Last summer, my little brother fell in a/an ${userInputNoun3} and got poison ${userInputPlant} all over his ${userInputBody}. My family is going to ${userInputPlace}, and I will practice ${userInputVerbIng4}. Parents need vacations more than kids because parents are always very ${userInputAdj2} and because they have to work ${userInputNumber} hours every day all year making enough ${userInputPluralNoun4} to pay for the vacation. </h1>`;
+
